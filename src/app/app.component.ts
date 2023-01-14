dba29e842ca38e1adfdf5df5e1040af8b0586951
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2023-01-14 19:36:22 +0500

    updated finalized v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,16 +15,37 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ngOnInit() {
+
+  }
+  ngDoCheck() {
+    console.log('ngOnChanges')
+    if (this.name.value && this.email.value && !this.email.invalid && this.phone.value && !this.isEdit) {
+    console.log('ngOnChanges inside if')
+
+      this.disabled = false
+    }
+    else{
+      this.disabled=true
+    }
+
+  }
+  checkinputFields() {
+
+
+  }
   name = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
   phone = new FormControl('', [Validators.required]);
   displayedColumns: string[] = ['name', 'email', 'phone', 'delete'];
-
-  data: any = []
+  data: any = [{ id: 123, name: 'Ahmed', email: 'ahmed@live.com', phone: 12223 },
+  { id: 321, name: 'Saad', email: 'saad@live.com', phone: 1221111 }
+  ]
   dataSource = this.data;
   genId = 0;
   isEdit: boolean = false;
   editRecords: any = {};
+  disabled: boolean = true;
 
   getErrorMessage() {
     if (this.email.hasError('required') || (this.name.hasError('required'))) {
@@ -34,45 +55,52 @@ export class AppComponent {
   }
   saveform() {
 
-    if (this.dataSource.length === 0) {
-      console.log('If')
-      this.genId = 0;
+    let id_gen = Math.floor(Math.random() * 1000);
+    if (id_gen > 99) {
+      let obj = {
+        id: id_gen,
+        name: this.name.value,
+        email: this.email.value,
+        phone: this.phone.value,
+        isEdit: this.isEdit,
+      }
+      let isExist=this.dataSource.find((v:any)=>v.email==obj.email)
+      console.log('isExist',isExist)
+      if(!isExist){
+        this.dataSource = [...this.dataSource, obj];
+      }
+      else{
+        alert('Record already exist')
+      }
     }
-    else {
-      console.log('else')
-      this.genId = (this.dataSource[this.dataSource.length - 1].id) + 1
-
-    }
-    console.log(this.genId, 'Iddd')
-    let obj = {
-      id: this.genId,
-      name: this.name.value,
-      email: this.email.value,
-      phone: this.phone.value,
-      isEdit: this.isEdit,
-    }
-    this.dataSource = [...this.dataSource, obj];
-    console.log(this.dataSource)
+    this.name.setValue('');
+    this.email.setValue('');
+    this.phone.setValue('');
+    this.disabled=true
   }
   delete(id: any) {
     this.dataSource = this.dataSource.filter((v: any) => v.id != id)
   }
   edit(id: any) {
-    this.isEdit = true
-    console.log('this', this.name.value)
-    let edit = this.dataSource.find((v: any) => v.id == id)
-    this.editRecords.name = edit.name
-    this.editRecords.email = edit.email
-    this.editRecords.phone = edit.phone,
-    this.editRecords.id=id
-
-    console.log('edit', edit)
+    this.isEdit = true;
+    this.disabled=true;
+    let findRecIndex = this.dataSource.findIndex((v: any) => v.id == id)
+    console.log('finalRec', this.disabled)
+    this.name.setValue(this.dataSource[findRecIndex].name);
+    this.email.setValue(this.dataSource[findRecIndex].email);
+    this.phone.setValue(this.dataSource[findRecIndex].phone);
+    this.dataSource[findRecIndex].isEdit=true
   }
-  update() {
+  update(id: any) {
     this.isEdit = false
-    let id=this.editRecords.id
-    this.dataSource[id]=this.editRecords
-    console.log('Update', this.dataSource)
+    let findRecIndex = this.dataSource.findIndex((v: any) => v.id == id)
+    this.dataSource[findRecIndex].name = this.name.value
+    this.dataSource[findRecIndex].email = this.email.value
+    this.dataSource[findRecIndex].phone = this.phone.value
+    this.dataSource[findRecIndex].isEdit=false
+    this.name.setValue('');
+    this.email.setValue('');
+    this.phone.setValue('');
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
-import { TableComponent } from './table/table.component';
-import { InputComponent } from './input/input.component';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatRadioModule} from '@angular/material/radio';
-import { RadiobtnComponent } from './radiobtn/radiobtn.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
-
-
-
-
-
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent,
-    InputComponent,
-    RadiobtnComponent
   ],
   imports: [
     BrowserModule,
